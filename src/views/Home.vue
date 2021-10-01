8482d5c0eb7129c8d72fdbddf982c9dd762e93cb
SalemNabeelSalem <isalemnabeelsalem@gmail.com>
2021-10-01 21:02:56 +0300

    Install the Bootstrap-Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,28 +3,64 @@
     <v-app>
       <v-container fluid class="pa-0">
         <v-row align="center">
-          <v-col cols="12" sm="12" class="my-12">
+          <v-col cols="12" sm="12" class="my-6">
             <div class="text-center">
-              <div class="my-12">
-                <v-btn x-large color="primary" dark href="/manager-dashboard">
-                  Manager Dashboard
-                </v-btn>
+              <div>
+                <b-card
+                  bg-variant="success"
+                  text-variant="light"
+                  title="Manager Dashboard"
+                >
+                  <b-card-text>
+                    With supporting manager dashboard as a natural lead-in to
+                    additional content.
+                  </b-card-text>
+                  <b-button href="/manager-dashboard" variant="light">
+                    Go
+                  </b-button>
+                </b-card>
               </div>
 
               <div class="my-12">
-                <v-btn x-large color="primary" dark> Doctor Dashboard </v-btn>
+                <b-card
+                  bg-variant="success"
+                  text-variant="light"
+                  title="Doctor Dashboard"
+                >
+                  <b-card-text>
+                    With supporting doctor dashboard as a natural lead-in to
+                    additional content.
+                  </b-card-text>
+                  <b-button href="#" variant="light">Go</b-button>
+                </b-card>
               </div>
 
               <div class="my-12">
-                <v-btn x-large color="primary" dark>
-                  Laboratory Dashboard
-                </v-btn>
+                <b-card
+                  bg-variant="success"
+                  text-variant="light"
+                  title="Laboratory Dashboard"
+                >
+                  <b-card-text>
+                    With supporting laboratory dashboard as a natural lead-in to
+                    additional content.
+                  </b-card-text>
+                  <b-button href="#" variant="light">Go</b-button>
+                </b-card>
               </div>
 
               <div class="my-12">
-                <v-btn x-large color="primary" dark>
-                  Receptionist Dashboard
-                </v-btn>
+                <b-card
+                  bg-variant="success"
+                  text-variant="light"
+                  title="Receptionist Dashboard"
+                >
+                  <b-card-text>
+                    With supporting receptionist dashboard as a natural lead-in
+                    to additional content.
+                  </b-card-text>
+                  <b-button href="#" variant="light">Go</b-button>
+                </b-card>
               </div>
             </div>
           </v-col>
